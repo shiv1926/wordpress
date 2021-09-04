a6419c51930a17bc14b1868970e655ae06109f43
shivkumar yadav <shiv.yadav26@gmail.com>
2021-09-04 11:21:59 +0530

    idnex

--- index.php
+++ index.php
@@ -1,8 +1,7 @@
 wordpress site performance : https://wordpress.org/support/article/optimization/
-
 how to create theme in wordpress
-
 create an script for downloading all the images from live site to local machine
 create custom search with number pagination
-
 what is amp and amp plugin
+how to use and configure CDN , free cdn
+SMTP plugin, cconfigure it
