cd0df4375265343e4f29b763a90f483cb2dac83b
Shiv Yadav <shiv@cibirix.com>
2021-09-15 21:41:38 +0530

    index

--- index.php
+++ index.php
@@ -5,3 +5,18 @@ create custom search with number pagination
 what is amp and amp plugin
 how to use and configure CDN , free cdn
 SMTP plugin, cconfigure it
+
+
+create variables in yoast seo plugin to dynamic content
+function get_myname() {
+    return 'My name is Moses';
+}
+
+function register_custom_yoast_variables() {
+    wpseo_register_var_replacement( '%%myname%%', 'get_myname', 'advanced', 'some help text' );
+}
+add_action('wpseo_register_extra_replacements', 'register_custom_yoast_variables');
+
+
+plugin handbook : https://developer.wordpress.org/plugins/
+http://www.billerickson.net/code/default-term-for-taxonomy/
